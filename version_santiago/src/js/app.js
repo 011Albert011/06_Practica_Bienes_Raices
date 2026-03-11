097e8f47f23a934a2c0d9fe259717776303cf158
Santiago <157559928+S4nt1t0@users.noreply.github.com>
2026-03-10 19:10:11 -0600

    resto de archivos sin node_modules V2

---
+++ version_santiago/src/js/app.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    eventListeners();
+    darkMode();
+});
+
+function darkMode() {
+    const preferenciaDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (preferenciaDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    preferenciaDarkMode.addEventListener('change', function() {
+         if (preferenciaDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');    
+    });
+}
+
+function eventListeners() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion');
+
+    navegacion.classList.toggle('mostrar');
+}
