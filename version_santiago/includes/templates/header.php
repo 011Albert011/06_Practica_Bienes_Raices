b3a30b6e84c6f46e4f042fdf67297e9daeeabab3
Albert K. <albertyunior80@gmail.com>
2026-03-14 21:36:13 -0600

    Add files via upload

--- version_santiago/includes/templates/header.php
+++ version_santiago/includes/templates/header.php
@@ -12,22 +12,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raíces</title>
 
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 <body>
     <header class="header <?php echo $inicio ? 'inicio' : ''; ?>">
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/">
-                    <img src="build/img/logo.svg" alt="Logotipo de Bienes Raíces">
+                    <img src="/build/img/logo.svg" alt="Logotipo de Bienes Raíces">
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                    <img src="/build/img/barras.svg" alt="icono menu responsive">
                 </div>
 
                 <div class="derecha">
-                    <img src="build/img/dark-mode.svg" class="dark-mode-boton">
+                    <img src="/build/img/dark-mode.svg" class="dark-mode-boton">
 
                     <nav class="navegacion">
                         <a href="nosotros.php">Nosotros</a>
